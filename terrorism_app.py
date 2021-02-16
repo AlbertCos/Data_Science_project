c3dc01cf2dbc0f8e3f747114fb1af654d1b4a239
AlbertCos <cosmenalberto@gmail.com>
2021-02-17 00:47:49 +0100

    **17/02/2021-BRK**

--- terrorism_app.py
+++ terrorism_app.py
@@ -11,12 +11,16 @@ def load_data(filename):
 df = load_data("global_terror.csv")
 st.title("Global Terrorism Exploration APP")
 
-#Create slider filter for panel exploration
+#Create sliders to filter for panel exploration
 col_filters, col_viz = st.beta_columns([1,3])
 with col_filters:
-    st.slider("Year", 0, 100, 50)
-with col_viz:
-    st.slider("Test", 0, 100, 50)
+    y_min = min(df["iyear"])
+    y_max = max(df["iyear"])
+
+    year = st.slider("Choose a Year:", y_min, y_max, (y_min,y_max))
+
+#Testing year slider choosed
+st.subheader(f"You have selected year: {year}!")
 
 #Test purposes
 st.write(df)
