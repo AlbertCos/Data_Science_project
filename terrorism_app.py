f074321959e297b9c19c8d997f8b5cdc10e543d2
AlbertCos <cosmenalberto@gmail.com>
2021-02-17 00:31:35 +0100

    Load Data and filters sliders added

--- terrorism_app.py
+++ terrorism_app.py
@@ -3,6 +3,15 @@ import streamlit as st
 
 @st.cache
 def load_data(filename):
+    keep_columns =["iyear", "imonth", "iday", "country_txt", "region_txt","latitude","longitude"]
+    df = pd.read_csv(filename,encoding="latin-1")  
+    df = df[keep_columns]
+    return df
 
+df = load_data("global_terror.csv")
 st.title("Global Terrorism Exploration APP")
 
+col_filters, col_viz = st.beta_columns([1,3])
+
+#Test purposes
+st.write(df)
