eb463530ed5c2cc42865a6d89e3703c1d763a7da
AlbertCos <cosmenalberto@gmail.com>
2021-02-17 22:46:59 +0100

    ****BR*** 17/02/2021*

--- terrorism_app.py
+++ terrorism_app.py
@@ -9,9 +9,15 @@ def load_data(filename):
     return df
 
 df = load_data("global_terror.csv")
-st.title("Global Terrorism Exploration APP")
 
-region = st.selectbox("Choose a Region", df["region_txt"].unique())
+st.title("Global Terrorism Exploration APP")
+# TODO: Add "all regions option"****
+
+regions = df["region_txt"].unique().tolist()
+st.write(type(regions))
+st.write(regions)
+
+region = st.selectbox("Choose a Region", regions)
 
 #Create sliders to filter for panel exploration
 col_filters, col_viz = st.beta_columns([1,3])
@@ -21,8 +27,11 @@ with col_filters:
     year = st.slider("Choose a Year:", y_min, y_max, (y_min,y_max))
 
     in_region=df["region_txt"] == region
+    #TODO: ADD "all countries option" ****
     country= st.selectbox("Choose a Country", options = df.loc[in_region,"country_txt"].unique())
 
+with col_viz:
+    chart_type = st.selectbox("Select type of viz: ", options = ["Line", "Histogram","Map"])
 
 #Testing year slider choosed
 st.subheader(f"You have selected year: {year}! and {country}")
