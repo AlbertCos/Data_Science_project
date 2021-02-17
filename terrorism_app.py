dcf1540d98a3242729d7c30ef311cc718513101f
AlbertCos <cosmenalberto@gmail.com>
2021-02-17 09:54:38 +0100

    Added selectbox for country and for region

--- terrorism_app.py
+++ terrorism_app.py
@@ -11,16 +11,21 @@ def load_data(filename):
 df = load_data("global_terror.csv")
 st.title("Global Terrorism Exploration APP")
 
+region = st.selectbox("Choose a Region", df["region_txt"].unique())
+
 #Create sliders to filter for panel exploration
 col_filters, col_viz = st.beta_columns([1,3])
 with col_filters:
     y_min = min(df["iyear"])
     y_max = max(df["iyear"])
-
     year = st.slider("Choose a Year:", y_min, y_max, (y_min,y_max))
 
+    in_region=df["region_txt"] == region
+    country= st.selectbox("Choose a Country", options = df.loc[in_region,"country_txt"].unique())
+
+
 #Testing year slider choosed
-st.subheader(f"You have selected year: {year}!")
+st.subheader(f"You have selected year: {year}! and {country}")
 
 #Test purposes
 st.write(df)
