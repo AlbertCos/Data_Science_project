6a8817a1950e7db6b017dc54e691e224677988f5
AlbertCos <cosmenalberto@gmail.com>
2021-02-24 22:02:54 +0100

    added two functions for pies charts

--- terrorism_app.py
+++ terrorism_app.py
@@ -15,7 +15,10 @@ def load_data(filename):
         "region_txt",
         "latitude",
         "longitude", 
-        "provstate"
+        "provstate",
+        "targtype1_txt",
+        "attacktype1_txt",
+        "gname"
         ]
 
     df = pd.read_csv(filename,encoding="latin-1")  
@@ -80,7 +83,7 @@ def pie_most_dangerous_cities (df,country):
     bad_cities.columns = ["City", "Number of Attacks"]
     total_attacks = bad_cities["Number of Attacks"].sum()
     try:
-        thresh = bad_cities["Number of Attacks"].iloc[9]
+        thresh = bad_cities["Number of Attacks"].iloc[6]
     except:
         thresh = 0
 
@@ -95,7 +98,48 @@ def pie_most_dangerous_cities (df,country):
         values = big_bad_cities["Number of Attacks"],
         labels = big_bad_cities["City"],
         type = "pie",
-        hole = 0.3
+        hole = 0.3,
+        showlegend=False
+    )
+    Layout = dict(
+        margin=dict(l=0,r=0,b=0,t=0),
+        width = 400,
+        height = 400,
+    )
+    
+    fig = go.Figure(
+        data= Data,
+        layout=go.Layout( Layout)
+    )
+
+    return fig
+
+def pie_most_attacked_targets (df,country):
+    country_filter = df["country_txt"] == country
+    filtered = df[country_filter]
+
+    attacked_targets = filtered["targtype1_txt"].value_counts().to_frame().reset_index()
+    attacked_targets.columns = ["Target", "Number of Attacks"]
+    total_attacks = attacked_targets["Number of Attacks"].sum()
+
+    try:
+        thresh = attacked_targets["Number of Attacks"].iloc[6]
+    except:
+        thresh = 0
+
+    main_target = attacked_targets[attacked_targets["Number of Attacks"]> thresh]
+    other = pd.Series({"Target":"Other",
+    "Number of Attacks":total_attacks - attacked_targets["Number of Attacks"].sum()})
+
+    if other["Number of Attacks"] !=0:
+        main_target= main_target.append(other, ignore_index=True)
+
+    Data = dict(
+        values = main_target["Number of Attacks"],
+        labels = main_target["Target"],
+        type = "pie",
+        hole = 0.3,
+        showlegend=False
     )
     Layout = dict(
         margin=dict(l=0,r=0,b=0,t=0),
@@ -110,6 +154,87 @@ def pie_most_dangerous_cities (df,country):
 
 
     return fig
+
+def pie_most_freq_type_attack (df,country):
+    country_filter = df["country_txt"] == country
+    filtered = df[country_filter]
+
+    attack_type = filtered["attacktype1_txt"].value_counts().to_frame().reset_index()
+    attack_type.columns = ["Attack Type", "Number of Attacks"]
+    total_attacks = attack_type["Number of Attacks"].sum()
+
+    try:
+        thresh = attack_type["Number of Attacks"].iloc[5]
+    except:
+        thresh = 0
+
+    main_type_attack = attack_type[attack_type["Number of Attacks"]> thresh]
+    other = pd.Series({"Attack Type":"Other",
+    "Number of Attacks":total_attacks - attack_type["Number of Attacks"].sum()})
+
+    if other["Number of Attacks"] !=0:
+        main_type_attack= main_type_attack.append(other, ignore_index=True)
+
+    Data = dict(
+        values = main_type_attack["Number of Attacks"],
+        labels = main_type_attack["Attack Type"],
+        type = "pie",
+        hole = 0.3,
+        showlegend=False
+    )
+    Layout = dict(
+        margin=dict(l=0,r=0,b=0,t=0),
+        width = 400,
+        height = 400,
+    )
+    
+    fig = go.Figure(
+        data= Data,
+        layout=go.Layout( Layout)
+    )
+
+    return fig
+
+def pie_most_active_groups (df,country):
+    country_filter = df["country_txt"] == country
+    filtered = df[country_filter]
+
+    terror_groups = filtered["gname"].value_counts().to_frame().reset_index()
+    terror_groups.columns = ["Terrorist Group", "Number of Attacks"]
+    total_attacks = terror_groups["Number of Attacks"].sum()
+
+    try:
+        thresh = terror_groups["Number of Attacks"].iloc[5]
+    except:
+        thresh = 0
+
+    main_terror_groups = terror_groups[terror_groups["Number of Attacks"]> thresh]
+    other = pd.Series({"Terrorist Group":"Other",
+    "Number of Attacks":total_attacks - terror_groups["Number of Attacks"].sum()})
+
+    if other["Number of Attacks"] !=0:
+        main_terror_groups= main_terror_groups.append(other, ignore_index=True)
+
+    Data = dict(
+        values = main_terror_groups["Number of Attacks"],
+        labels = main_terror_groups["Terrorist Group"],
+        type = "pie",
+        hole = 0.3,
+        showlegend=False
+    )
+    Layout = dict(
+        margin=dict(l=0,r=0,b=0,t=0),
+        width = 400,
+        height = 400,
+    )
+    
+    fig = go.Figure(
+        data= Data,
+        layout=go.Layout( Layout)
+    )
+
+    return fig
+
 
 df = load_data("Terrorism_clean_dataset.csv")
 
@@ -130,9 +255,9 @@ country= st.sidebar.selectbox("Choose a Country", options = countries)
 
 is_country = df["country_txt"] == country
 
-st.title(f"Terrorism Brief:{country}")
+st.title(f"Terrorism Brief: {country}")
 
-col_info, col_viz = st.beta_columns ([1,1.6])
+col_info, col_viz = st.beta_columns ([1,1.8])
 
 with col_info:
     #Most Attacked City
@@ -156,13 +281,23 @@ with col_info:
     st.subheader("Year of Fewest Attacks:")
     st.markdown(f"*{best_year} with {num_attacks_best_year} attacks*")
 
+isyear=df["iyear"].unique().tolist()
+isyear.insert(0,"All history")
+
 with col_viz:
-    plot_type = st.selectbox("Choose a visualization:", options = ["Histogram: Attacks Over Time", "Pie: Most Dangerous Cities"])
+    plot_type = st.selectbox("Choose a visualization:", options = ["Histogram: Attacks Over Time", "Pie: Most Dangerous Cities","Pie: Most Attacked Targets","Pie: Most Frequent Type of Attack", "Pie: Main Terrorist Groups"])
 
     if plot_type == "Histogram: Attacks Over Time":
         st.plotly_chart(hist_attacks_over_time(df,country),width=300 , height=400, margin=dict(l=0, r=0, b=0, t=0),autosize=False,)
     elif plot_type == "Pie: Most Dangerous Cities":
         st.plotly_chart(pie_most_dangerous_cities(df,country), width=300 , height=400, margin=dict(l=0, r=0, b=0, t=0),autosize=False,)
+    elif plot_type == "Pie: Most Attacked Targets":
+        st.plotly_chart(pie_most_attacked_targets(df,country),width=300 , height=400, margin=dict(l=0, r=0, b=0, t=0),autosize=False,)
+    elif plot_type == "Pie: Most Frequent Type of Attack":
+        st.plotly_chart(pie_most_freq_type_attack(df,country),width=300 , height=400, margin=dict(l=0, r=0, b=0, t=0),autosize=False,)
+    elif plot_type == "Pie: Main Terrorist Groups":
+        st.plotly_chart(pie_most_active_groups(df,country),width=300 , height=400, margin=dict(l=0, r=0, b=0, t=0),autosize=False,)
+        st.text("the main Terrorist Groups in spain")
 
 st.subheader(f"{country}: Nationwide Attacks over Time")
 st.plotly_chart(line_attacks_over_time(df,country))
